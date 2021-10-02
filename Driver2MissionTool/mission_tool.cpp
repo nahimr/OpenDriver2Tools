3eea4510c7349c1313a154885adda74817d72608
Nahim <nahim.rouidjali@gmail.com>
2021-10-02 22:12:20 +0200

    Add ParseTargets

--- Driver2MissionTool/mission_tool.cpp
+++ Driver2MissionTool/mission_tool.cpp
@@ -120,10 +120,72 @@ MS_MISSION ParseSettings(xml_node<>* el, StringsStack* strings)
 	return settings;
 }
 
-MS_TARGET* ParseTargets(xml_node<>* element)
+MS_TARGET* ParseTargets(xml_node<>* element, StringsStack* strings)
 {
-	printf("%s\n", element->name());
 	MS_TARGET* targets = static_cast<MS_TARGET*>(malloc(sizeof(MS_TARGET) * 16));
+	xml_node<>* el = element->first_node();
+	for (int i = 0; i < 16; i++)
+	{
+		if (el == nullptr) break;
+		std::string name = el->name();
+		// Parse Targets
+		if (name == "Car" || name == "Player2Start")
+		{
+			targets[i].type = name == "Car" ? Target_Car : Target_Player2Start;
+			targets[i].target_flags = mapValues(split(el->first_attribute("targetFlags")->value(), '|'));
+			targets[i].display_flags = mapValues(split(el->first_attribute("displayFlags")->value(), '|'));
+			targets[i].car.posX = atoi(el->first_attribute("posX")->value());
+			targets[i].car.posZ = atoi(el->first_attribute("posZ")->value());
+			targets[i].car.rotation = atoi(el->first_attribute("rotation")->value());
+			targets[i].car.slot = atoi(el->first_attribute("slot")->value());
+			targets[i].car.model = atoi(el->first_attribute("carModel")->value());
+			targets[i].car.palette = atoi(el->first_attribute("carColour")->value());
+			targets[i].car.cutscene = atoi(el->first_attribute("cutscene")->value());
+			targets[i].car.flags = mapValues(split(el->first_attribute("flags")->value(), '|'));
+			targets[i].car.type = 0;
+
+			if (el->first_node("Chasing"))
+			{
+				el = el->first_node("Chasing");
+				targets[i].car.type = 2;
+				targets[i].car.chasing.tooFarMessage = strings->getOffset(el->first_attribute("tooFarMessageStrId")->value());
+				targets[i].car.chasing.gettingFarMessage = strings->getOffset(el->first_attribute("gettingFarMessageStrId")->value());
+				targets[i].car.chasing.maxDamage = atoi(el->first_attribute("maxDamage")->value());
+			}
+			else if (el->first_node("Tailing"))
+			{
+				el = el->first_node("Tailing");
+				targets[i].car.type = 1;
+				targets[i].car.tail.closeMessages = strings->getOffset(el->first_attribute("closeMessageStrId")->value());
+				targets[i].car.tail.farMessages = strings->getOffset(el->first_attribute("farMessageStrId")->value());
+			}
+		}
+		else if (name == "Point")
+		{
+			targets[i].type = Target_Point;
+			targets[i].target_flags = mapValues(split(el->first_attribute("targetFlags")->value(), '|'));
+			targets[i].display_flags = mapValues(split(el->first_attribute("displayFlags")->value(), '|'));
+			targets[i].point.posX = atoi(el->first_attribute("posX")->value());
+			targets[i].point.posZ = atoi(el->first_attribute("posZ")->value());
+			targets[i].point.posY = atoi(el->first_attribute("posY")->value());
+			targets[i].point.height = atoi(el->first_attribute("height")->value());
+			targets[i].point.radius = atoi(el->first_attribute("radius")->value());
+			targets[i].point.loseTailMessage = strings->getOffset(el->first_attribute("loseTailMessageStrId")->value());
+			targets[i].point.actionFlag = mapValues(split(el->first_attribute("flags")->value(), '|'));
+			targets[i].point.boatOffsetX = atoi(el->first_attribute("boatOffsetX")->value());
+			targets[i].point.boatOffsetZ = atoi(el->first_attribute("boatOffsetZ")->value());
+		}
+		else if (name == "Event")
+		{
+			
+		}
+		else if (name == "MultiCar")
+		{
+			
+		}
+
+		el = element->next_sibling();
+	}
 	return targets;
 }
 
@@ -139,11 +201,12 @@ void CreateMission(std::string &filename)
 	file<> file(filename.data());
 	xml_document<> document;
 	document.parse<0>(file.data());
-	xml_node<>* node = document.first_node()->first_node();
+	xml_node<>* node = document.first_node();
+	int missionNumber = atoi(node->first_attribute("id")->value());
 
-	StringsStack* strings = ParseStrings(node);
+	StringsStack* strings = ParseStrings(node = node->first_node());
 	MS_MISSION settings = ParseSettings(node = node->next_sibling(), strings);
-	MS_TARGET* targets = ParseTargets(node = node->next_sibling());
+	MS_TARGET* targets = ParseTargets(node = node->next_sibling(), strings);
 	Stack stack = ParseScript(node = node->next_sibling());
 	
 	free(targets);
